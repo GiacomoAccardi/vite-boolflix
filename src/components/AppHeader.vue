18523af0182add0410cf8ef501d7c1d339df4d34
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-08-26 16:55:10 +0200

    [UPD] There's now a searchbar and a button in the header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 
 <script>
-import { store } from './store';
+import { store } from '../store';
 
 export default{
     data() {
@@ -12,7 +12,30 @@ export default{
 </script>
 
 <template>
+    <header>
+        <!-- Questo conterrà l'header del nostro sito web -->
+        <div class="container align-items">
+
+            <!-- colonna che conterrà il logo -->
+            <div class="col">
+                <h1>Boolflix</h1>
+            </div>
+            <!-- colonna che conterrà la search bar -->
+            <div class="col">
+                <input type="text">
+                <button>cerca</button>
+            </div>
+
+        </div>
+    </header>
 </template>
 
 <style lang="scss">
+header{
+    background-color: grey;
+
+    .col {
+        width: 50%;
+    }
+}
 </style>
