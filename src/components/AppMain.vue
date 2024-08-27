f5bb402da668988ae889fb1d7c377bf836edf9c7
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-08-27 10:39:22 +0200

    [FEAT] The app now shows flags for each language. [FEAT] you can now use the enter key for search in the search bar (you can still use the button)

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,64 +1,81 @@
 
 <script>
 import { store } from '../store';
+import LangFlag from 'vue-lang-code-flags';
 
 export default {
     data() {
         return {
             store
         }
-    }
+    },
+    components: {
+    LangFlag,
+  },
 }
 </script>
 
 <template>
-    <div class="container-sm">
-            <!-- qui andranno le locandine dei film  -->
+
     <main>
-        <div class="box" v-for="film in store.filmList">
-            <ul>
-                <li>
-                    <span><strong>Titolo: </strong> {{ film.title }} </span>
-                </li>
+        <!-- qui andranno le locandine dei film  -->
+        <div class="container-sm">
 
-                <li>
-                    <span><strong>Titolo originale: </strong> {{ film.original_title }} </span>
-                </li>
+            <div class="content">
+                <!-- card per i film -->
+                <div class="box" v-for="film in store.filmList">
+                    <ul>
+                        <li>
+                            <span><strong>Titolo: </strong> {{ film.title }} </span>
+                        </li>
 
-                <li>
-                    <span><strong>Voto: </strong> {{ film.vote_average }} </span>
-                </li>
+                        <li>
+                            <span><strong>Titolo originale: </strong> {{ film.original_title }} </span>
+                        </li>
 
-                <li>
-                    <span><strong>Lingua: </strong> {{ film.original_language }} </span>
-                </li>
-            </ul>
+                        <li>
+                            <span><strong>Voto: </strong> {{ film.vote_average }} </span>
+                        </li>
+
+                        <li>
+                            <strong>Lingua originale: </strong>
+                            <lang-flag :iso="film.original_language" />
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <!-- card per i telefilm -->
+
         </div>
     </main>
-    </div>
+
 </template>
 
 <style lang="scss">
     main{
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
         margin-top: 50px;
 
-        .box{
-            width: 250px;
-            min-height: 350px;
-            background-color: black;
-            padding: 20px;
-            color: white;
-            font-size: 10pt;
-            margin: 10px;
+        .content{
+            display: flex;
+            flex-wrap: wrap;
 
-            ul{
-                list-style: none;
-
-                li {
-                    margin-top: 5px;
+            .box{
+                width: 250px;
+                min-height: 350px;
+                background-color: black;
+                padding: 20px;
+                color: white;
+                font-size: 10pt;
+                margin: 10px;
+    
+                ul{
+                    list-style: none;
+    
+                    li {
+                        margin-top: 5px;
+                    }
                 }
             }
         }
